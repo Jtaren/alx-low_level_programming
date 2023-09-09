ebc8ba2189ecb4e8e6676aa9fe21c177f53d7bb0
jtaren <jatauaren@gmail.com>
2023-09-09 11:44:08 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,24 +12,22 @@ int main(void)
 	{
 		for (m = 49; m < 58; m++)
 		{
-              		for (l = 50; l < 58; l++)
+			for (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
 				{
-                                        putchar(n);
-                                        putchar(m);
-                                        putchar(l);
-                           		if (n != 55 || m != 56)
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
 					{
-                                                putchar(',');
+						putchar(',');
 						putchar(' ');
-                                      	}
-                              	}
-                      	}
-                }
-
-        }
-        putchar('\n');
-        return (0);
-}   
-
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
