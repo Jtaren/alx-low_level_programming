95c0699feaba095594c83ca8890246fb0f68e9a2
jtaren <jatauaren@gmail.com>
2023-09-09 07:52:44 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz":
-	int i:
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	for (i = 0: i < 26: i++)
+	for (i = 0; i < 26; i++)
 	{
-		putcahr(alp[i]):
+		putchar(alp[i]);
 	}
-	putchar('\n'):
-	return (0):
+	putchar('\n');
+	return (0);
 }
