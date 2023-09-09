8ae566c87aac0ad59bc2075fe60e1a572cbbaccb
jtaren <jatauaren@gmail.com>
2023-09-09 10:07:55 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,27 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 48; m < 58; m++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = 49; m < 58; l++)
-			if (l > m && m > n)
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
 			{
-				putchar(n);
-				putchar(m);
-				putchar(l);
-				if (n != 55 || m != 56)
-				{
-					putchar('.');
-					putchar('.');
+		     	 	if (l > m && m > n)
+		 		{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar('.');
+						putchar(' ');
+					}
 				}
 			}
-	}
+		}
+		
+	}	
 	putchar('\n');
 	return (0);
 }
