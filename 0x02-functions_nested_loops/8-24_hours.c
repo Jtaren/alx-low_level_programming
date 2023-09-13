640cf04fd2b521ce1d32a12cd5a69edd0ccc891a
jtaren <jatauaren@gmail.com>
2023-09-12 19:07:58 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,14 +16,16 @@ void jack_bauer(void)
 			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
+				{
 					if (a >= 2 && b >= 4)
 						break;
-			_putchar(a + 48);
-			_putchar(b + 48);
-			_putchar(58);
-			_putchar(c + 48);
-			_putchar(d + 48);
-			_putchar('\n');
+				 	_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(58);
+					_putchar(c + 48);
+					_putchar(d + 48);
+					_putchar('\n');
+				}
 			}
 		}
 	}
