d4dd051ec6169543bb41bd2b6dd7b0eb6beb556e
jtaren <jatauaren@gmail.com>
2023-09-12 18:56:43 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints the minutes of a day
+ *
+ * Returns: no return
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 0; b++)
+		{
+			for (c = 0; c <= 7; c++)
+				for (d = 0; d <= 9; d++)
+					if (a >= 2 && b >= 4)
+						break;
+			_putchar(a + 48);
+			_putchar(b + 48);
+			_putchar(58);
+			_putchar(c + 48);
+			_putchar(d + 48);
+			_putchar('\n');
+		}
+	}
+}
