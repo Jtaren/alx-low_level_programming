c13e60b4e45cf399af0441818105f8d76752c1e8
jtaren <jatauaren@gmail.com>
2023-09-15 21:24:49 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ *times_table - Prints the 0 times table, starting with 0
+ * Return: empty output
  */
+
 void times_table(void)
+
 {
 	int x, y, z, u, d;
 
