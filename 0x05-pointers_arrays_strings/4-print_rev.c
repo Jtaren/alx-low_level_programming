0db2d5e1c1e4a0bf3ad500a46bf8a3fb66da0ffb
jtaren <jatauaren@gmail.com>
2023-09-18 19:38:07 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,19 +6,15 @@
  */
 void print_rev(char *s)
 {
-	int longi = 0;
-	int o;
+	int c = 0;
 
-	while (*s != '\0')
+	while (s[c] != '\0')
 	{
-		longi++;
-		s++;
+		c++;
 	}
-	s--;
-	for (o = longi; o > 0; o--)
+	for (c -= 1; c >= 0; c--)
 	{
-		_putchar(*s);
-		s--;
+		_putchar(s[c]);
 	}
 	_putchar('\n');
 }
