c9d9ce229555890ac1851cd6b0ca5390a0ed38e6
jtaren <jatauaren@gmail.com>
2023-09-22 08:21:01 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *n);
+char *rot13(char *);
+void print_number(int n);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif
