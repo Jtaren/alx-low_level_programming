b8915c92eccaa30381991a386e43104a7ea335d3
jtaren <jatauaren@gmail.com>
2023-09-22 08:40:38 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,4 +17,6 @@ char *rot13(char *);
 void print_number(int n);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
 #endif
+}
