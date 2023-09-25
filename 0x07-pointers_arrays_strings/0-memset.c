091f01b10448b426713842c3ecd04238c33b9cd1
jtaren <jatauaren@gmail.com>
2023-09-25 11:04:49 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * memsel - fill a block of memory with a specific value
+ * @s starting address of memory to be filled
+ * @b the desired value
+ * @n number of bytes to be changed
+ *
+ * Return changed array with new values for n bytes
+ */
+char *_memsrt(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
