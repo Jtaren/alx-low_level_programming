035cc76520845d99db74fc0db4579c73aa474d9b
Jatau Aren <jatauaren@gmail.com>
2023-09-27 14:21:52 +0200

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,7 +11,7 @@ int _strlen_recursion(char *s);
  */
 int is_palindrome(char *s)
 {
-	if (*s -- 0)
+	if (*s == 0)
 		return(0);
 	return (check_pal(s, 0, _strlen_recursion(s)));
 }
@@ -23,7 +23,7 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-	if (*s -- '\0')
+	if (*s ==  '\0')
 		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
