51db6d9fb7284b4342b12db4ea8ab84f0eb6e4f2
jtaren <jatauaren@gmail.com>
2023-09-28 10:49:03 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _putsrecursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{       
+        if (*s)
+        {       
+                _putchar(*s);
+		Puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
