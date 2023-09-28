0db0174d182b569c8f7a4d4af05666eb8275e9f1
Jatau Aren <jatauaren@gmail.com>
2023-09-28 15:41:29 +0200

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -28,6 +28,6 @@ int actual_sqrt_recursion(int n, int i)
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
-                return (i);
+		return (i);
 	return (actual_sqrt_recursion(n, i + 1));
 }
