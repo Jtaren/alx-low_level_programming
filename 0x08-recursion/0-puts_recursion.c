552c396e23e8e01b369a3ba064a090cc8e481540
jtaren <jatauaren@gmail.com>
2023-09-28 10:55:05 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,10 +5,10 @@
  * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
-{       
-        if (*s)
-        {       
-                _putchar(*s);
+{
+	if (*s)
+	{
+		_putchar(*s);
 		Puts_recursion(s + 1);
 	}
 	else
