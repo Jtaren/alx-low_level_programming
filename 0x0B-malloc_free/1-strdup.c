f7821358050a962fbfc3319216ad2a57c8184de8
jtaren <arenjatau@gmail.com>
2023-10-03 06:03:10 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,14 +12,14 @@ char *_strdup(char *str)
 	int i, r = 0;
 
 	if (str == NULL)
-		return(NULL);
+		return (NULL);
 	i = 0;
 	while (str[i] != '\0')
 		i++;
 
 	aaa = malloc(sizeof(char) * (i + 1));
 
-	if(aaa == NULL)
+	if (aaa == NULL)
 		return (NULL);
 
 	for (r = 0; str[r]; r++)
