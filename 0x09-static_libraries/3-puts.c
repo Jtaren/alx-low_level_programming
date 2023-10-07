d3f0647c5d6ada2eeb301e0c7e48209cef3e97ea
jtaren <arenjatau@gmail.com>
2023-10-06 18:00:53 -0700

    3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,12 +2,13 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
+ * _putchar prints a new line
  */
 void _puts(char *str)
 {
-	while (str != '\0')
+	while (*str)
 	{
 		_putchar(*str++);
 	}
-		_putchar('\0');
+		_putchar('\n');
 }
