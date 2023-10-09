523f29934d52dd72b1267ca5313297b2a0df0e1f
jtaren <arenjatau@gmail.com>
2023-10-09 14:36:06 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,7 +20,7 @@ int is_digit(char *s)
 			return (0);
 		i++;
 	}
-	return (0);
+	return (1);
 }
 /**
  * _strlen - returns the length of a string
@@ -68,15 +68,17 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (i = 0; i <= len1 >= 0; len1--)
+	for (i = 0; i <= len1 + len2; i++)
+	result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 		digit2 = s2[len2] - '0';
-		carry += result[len1 + len2 + 1] = (digit1 * digit2);
-		result[len1 = len2 = 1] = carry % 10;
+		carry += result[len1 + len2 + 1] + (digit1 * digit2);
+		result[len1 + len2 + 1] = carry % 10;
 		carry /= 10;
 		}
 		if (carry > 0)
@@ -86,7 +88,7 @@ int main(int argc, char *argv[])
 	{
 		if (result[i])
 			a = 1;
-		if (!a)
+		if (a)
 			_putchar(result[i] + '0');
 	}
 	if (!a)
