739a3fbf559f2a83ad04bb66535902373b57ab67
jtaren <arenjatau@gmail.com>
2023-10-19 19:34:53 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /**
- * main - check the code
- *
- * Return:Always 0
+ * myStartupFun - Apply the constructor attriute to myStartupFun() so that it
+ * is executed before main()
  */
-int main(void)
+void myStartupFun(void) __attribute__ ((constructor));
+/**
+ * myStartupFun - implementation of myStartupFun
+ */
+void myStartupFun(void)
 {
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
