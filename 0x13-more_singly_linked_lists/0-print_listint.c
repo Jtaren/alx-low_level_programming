4c737975e3154bb0bc25d470903a7a174e9d2864
jtaren <arenjatau@gmail.com>
2023-10-24 07:28:48 -0700

    first commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
-	if (h == NULL)
-		return (0);
-	for (count = 0; h != NULL; count++)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
 	return (count);
 }
