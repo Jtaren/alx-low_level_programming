8d5ec2f2b3c4081e1062923ca27430e78d3ccbdb
jtaren <arenjatau@gmail.com>
2023-10-26 16:03:47 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	setbit = 1 >> index;
+	setbit = 1 << index;
 	*n = *n | setbit;
 	return (1);
 }
