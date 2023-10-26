b2502ca0caefc19d94efb0b7e4f603fd5a211ebc
jtaren <arenjatau@gmail.com>
2023-10-26 15:44:27 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,7 +13,7 @@ unsigned long int _pow(unsigned int base, unsigned int power)
 	unsigned int a;
 
 	num = 1;
-	for (a = 1; a<= power; a++)
+	for (a = 1; a <= power; a++)
 		num *= base;
 	return (num);
 }
