b00cb55da82ba520e1ecd816324c7be61e8e15ae
jtaren <arenjatau@gmail.com>
2023-10-30 19:47:56 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void check_IO_stat(int stat, int fd, char *filename, char mode);
+/**
+ * main - funct copies the content of one file to another
+ * @argc: The argument count
+ * @argv: The argument passed
+ *
+ * Return: 1 on success, exit otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int src, dest, n_read = 1024, wrote, close_src, close_dest;
+	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char  buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	src = open(argv[1], O_RDONLY);
+	check_IO_stat(src, -1, argv[1], 'o');
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	check_IO_stat(dest, -1, argv[2], 'W');
+	while (n_read == 1024)
+	{
+		n_read = read(src, buffer, sizeof(buffer));
+		if (n_read == -1)
+			check_IO_stat(-1, -1, argv[1], 'o');
+		if (wrote == -1)
+			check_IO_stat(-1, -1, argv[2], 'w');
+	}
+	close_src = close(src);
+	check_IO_stat(close_src, src, NULL, 'c');
+	close_dest = close(dest);
+	check_IO_stat(close_dest, dest, NULL, 'c');
+	return (0);
+}
+
+/**
+ * check_IO_stat - funct checks if a file can be opened or closed
+ * @stat: file descriptor of file to be opened
+ * @filename: name of file
+ * @mode: closing or opening
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check_IO_stat(int stat, int fd, char *filename, char mode)
+{
+	if (mode == 'c' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "ERROR: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	else if (mode == 'o' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(100);
+	}
+	else if (mode == 'w' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "ERROR: Can't write to %s\n", filename);
+	}
+}
