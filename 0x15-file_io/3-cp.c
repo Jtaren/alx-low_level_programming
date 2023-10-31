82573ade09e2c8e334b791b9fa5816df52a26e6a
jtaren <arenjatau@gmail.com>
2023-10-30 21:21:35 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,22 +25,22 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	src = open(argv[1], O_RDONLY);
-	check_IO_stat(src, -1, argv[1], 'o');
+	check_IO_stat(src, -1, argv[1], 'O');
 	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	check_IO_stat(dest, -1, argv[2], 'W');
 	while (n_read == 1024)
 	{
 		n_read = read(src, buffer, sizeof(buffer));
 		if (n_read == -1)
-			check_IO_stat(-1, -1, argv[1], 'o');
+			check_IO_stat(-1, -1, argv[1], 'O');
 		wrote = write(dest, buffer, n_read);
 		if (wrote == -1)
-			check_IO_stat(-1, -1, argv[2], 'w');
+			check_IO_stat(-1, -1, argv[2], 'W');
 	}
 	close_src = close(src);
-	check_IO_stat(close_src, src, NULL, 'c');
+	check_IO_stat(close_src, src, NULL, 'C');
 	close_dest = close(dest);
-	check_IO_stat(close_dest, dest, NULL, 'c');
+	check_IO_stat(close_dest, dest, NULL, 'C');
 	return (0);
 }
 
