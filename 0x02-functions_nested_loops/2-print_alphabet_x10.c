b934e7b6e8b1ca55fab7c7829a11a699fdef2215
jtaren <jatauaren@gmail.com>
2024-01-27 09:47:49 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print-alphabet_x10 - Prints the alphabet 10 times,
+ * print_alphabet_x10 - Prints the alphabet 10 times,
  * followed by a new line
  *
  * Return: Always 0 (Success)
